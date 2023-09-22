5991dd3e7a19de4d4c69a9aac8a5a3abcaad0b49
exclud <shammahnjomo12@gmail.com>
2023-09-22 15:52:16 +0300

    Fix the manifest to kill a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,17 +1,5 @@
 # Puppet manifest to kill a process named "killmenow" using pkill
 
-# Ensure the "pkill" command is available (you can omit this if it's already installed)
-package { 'procps':
-  ensure => 'installed',
-}
-
-# Define an exec resource to kill the process
-exec { 'kill_killmenow':
-  command     => 'pkill killmenow',
-  refreshonly => true,
-}
-
-# Notify the exec resource to run when needed
-notify { 'Kill killmenow process':
-  require => Exec['kill_killmenow'],
+exec{'pkill -f killmenow':
+path  => '/usr/bin/:/usr/local/bin/:/bin/'
 }

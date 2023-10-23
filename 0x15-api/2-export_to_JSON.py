a63beca0bf98ff3752f9d0775456a1f330c11583
exclud <shammahnjomo12@gmail.com>
2023-10-23 22:47:54 +0300

    PycodeStyle Correction

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
 """Fetch and Record todo list files"""
 
-
 import json
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
-    """Fetch and record the TODO list progress for a given employee ID from a REST API."""
-    
+    """Fetch and record the TODO list progress for a given employee ID."""
     # Endpoints
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    
+    todos_url = (
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    )
+
     # Fetch employee details
     user_response = requests.get(user_url)
     user_data = user_response.json()
-    
+
     # Fetch todos for the employee
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
-    
+
     # Filter completed tasks for console output
     done_tasks = [task for task in todos_data if task.get('completed')]
-    
+
     # Print the employee TODO list progress to the console
-    print(f"Employee {user_data.get('name')} is done with tasks({len(done_tasks)}/{len(todos_data)}):")
+    print(f"Employee {user_data.get('name')} is done with "
+          f"tasks({len(done_tasks)}/{len(todos_data)}):")
     for task in done_tasks:
         print("\t " + task.get('title'))
 
@@ -38,7 +40,6 @@ def get_employee_todo_progress(employee_id):
         }
         for task in todos_data
     ]
-    
     json_data = {str(employee_id): tasks_list}
 
     # Write all tasks to a JSON file
@@ -46,6 +47,7 @@ def get_employee_todo_progress(employee_id):
         json.dump(json_data, json_file, indent=4)
 
     print(f"Data exported to {employee_id}.json")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -57,5 +59,5 @@ if __name__ == "__main__":
     except ValueError:
         print("Employee ID must be an integer.")
         sys.exit(1)
-    
+
     get_employee_todo_progress(employee_id)
